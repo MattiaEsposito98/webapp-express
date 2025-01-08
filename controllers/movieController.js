39fe2a1d0e7cad619024755db4261968faec338d
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-01-08 16:21:25 +0100

    add reviews alla show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -12,15 +12,29 @@ function index(req, res, next) {
 
 function show(req, res, next) {
   const { id } = req.params
-  const movieSql = 'SELECT * FROM db_movies.movies WHERE id = ?'
 
+  //query per trovare un film
+  const movieSql = 'SELECT * FROM db_movies.movies WHERE id = ?'
   connection.query(movieSql, [id], (err, results) => {
     if (err) return next(err) // Interviene middlware errorsHandler per gestire l'errore
     if (results.length === 0) {
       return next()  // Passa al prossimo middleware notFound perché non ci sono erorri da gestire
     }
     const movie = results[0]
-    res.json(movie)
+
+
+
+    //query per le reviews
+    const reviewsSql = `SELECT * FROM db_movies.reviews WHERE movie_id = ?`
+    connection.query(reviewsSql, [id], (err, reviewsResults) => {
+      if (err) return next(err)
+      if (reviewsResults.length === 0) {
+        return next()
+      }
+      movie.reviews = reviewsResults
+      res.json(movie)
+    })
+
   })
 }
 
