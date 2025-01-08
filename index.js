ed8bd1d7ab69e350a115fbd32fb7bb6a192dcab4
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-01-08 14:39:32 +0100

    add variabiali ambientali e avvio server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Server")
+const express = require("express")
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 3000
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+)
+
+app.use(express.static('pubblic'))
+
+app.get('/', (req, res) => {
+  res.send('Server is running')
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
