1fa9333b3b271fa444361ca82c7c0ab381a706df
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-01-08 15:29:52 +0100

    rename database

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,7 @@
 const connection = require('../data/db')
 
 function index(req, res) {
-  let sql = 'SELECT * FROM movies.movies'
+  let sql = 'SELECT * FROM db_movies.movies;'
 
   connection.query(sql, (err, movies) => {
     if (err) return res.status(500).json({ message: err.message })
